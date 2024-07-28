0c6a37d26350f7ab408d95be82c628649eefe50a
zzm7j <moh8mm5d77@gmail.com>
2024-07-28 17:45:16 +0300

    Add files via upload

---
+++ my_scripts_kali/test1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+printf "welcome, we just want to cheack if u was the original user !\n"
+
+read -t 5  -p  "username :" user
+read  -t 3  -s -p  "password :" password
+
+printf "\nhello $user"
+
---
+++ my_scripts_kali/test6.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+
+printf "choose a number to a randomly greeting :\n1\n2\n3\n"
+read number
+
+
+
+if [ $number -eq 1 ]
+then
+	printf "welcome sewet heart !"
+
+fi
+
+if [ $number -eq 2 ]
+then 
+	printf "welcome mr.user"
+fi
+
+if [ $number -eq 3 ]
+then
+	echo welcome mother fucker
+fi 
+
