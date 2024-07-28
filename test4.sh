1cac6339f28211d41292b16e2bc8ab67be881c55
zzm7j <moh8mm5d77@gmail.com>
2024-07-28 17:49:34 +0300

    Delete test4.sh

--- test4.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-printf "Welcome to the counter !\n"
-read -p "Enter ur name :" name
-len=`expr length $name`
-
-
-printf "[*]charchters number :`expr length $name`\n[*]the first character:`expr substr $name 1 1`\n[*]the last character :`expr substr $name $len $len`"
